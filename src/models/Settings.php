0c733a3cc38dca8461c00665ac0ffed93cbcdfd7
lukeyouell <>
2018-12-10 14:18:25 +0000

    settings

---
+++ src/models/Settings.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace lukeyouell\queuemanager\models;
+
+use Craft;
+use craft\base\Model;
+
+class Settings extends Model
+{
+    // Public Properties
+    // =========================================================================
+
+    public $jobLimit = 200;
+
+    // Public Methods
+    // =========================================================================
+
+    public function rules()
+    {
+        return [
+            [['jobLimit'], 'integer']
+        ];
+    }
+}
